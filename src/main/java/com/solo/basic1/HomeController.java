d6f36083835927db95ab5e772d650f6a9526018f
falsolena <sychs022@gmail.com>
2024-05-16 16:43:20 +0900

    C:/Program Files/Git/home/plus

--- src/main/java/com/solo/basic1/HomeController.java
+++ src/main/java/com/solo/basic1/HomeController.java
@@ -2,6 +2,7 @@ package com.solo.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -36,5 +37,15 @@ public class HomeController {
     count++;
     return count;
   }
+  @GetMapping("home/plus")
+  @ResponseBody
+  //requestParam의 의미
+  // 개발자가 스프링부트에게 말한다.
+  // int a는 쿼리스트링에서 a파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+  // @RequestParam이라는 값이 생략되어있음
+  public int showPlus(@RequestParam(defaultValue = "0") int a,@RequestParam(defaultValue = "0") int b) {
+    return a + b;
+  }
 
+// home/plus?a=10&b=20
 }
